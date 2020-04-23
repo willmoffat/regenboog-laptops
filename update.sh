233cbf6e148104754c4b355fc45a54a264435a7e
Will Moffat <will@hamstersoup.com>
2020-04-23 12:53:37 +0200

    Updater: Record alive once a day.

--- update.sh
+++ update.sh
@@ -1,5 +1,28 @@
 #! /bin/bash
 
-echo 'Dummy update script.'
-date >> /tmp/update.log
-whoami >> /tmp/update.log
+LOG=/tmp/update.log
+
+heartbeat() {
+    FORM='1FAIpQLSemeGH8Ko9eR_Ri8UFyq0PtlC4WThPvrEX3AME0hp7h6JKJrg'
+    URL=https://docs.google.com/forms/d/e/$FORM/formResponse
+    # Post to Google Form. Only show status code.
+    curl $URL \
+         -s -o /dev/null -w "%{http_code}" \
+         -d ifq \
+         -d entry.1405426464="$HOSTNAME" \
+         -d submit=Submit
+}
+
+once_per_day() {
+    heartbeat
+}
+
+run() {
+    echo 'Update script.'
+    if [ ! -r $LOG ] ; then
+        once_per_day
+    fi
+    date >> $LOG
+}
+
+run
